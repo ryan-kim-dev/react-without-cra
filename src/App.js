0ec6cc19e453b6c25e59da144d0866ff1978f046
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-07-25 22:42:37 +0900

    feat: Implement React App without CRA

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>CRA 없이 리액트 앱 만들기</h1>
+    </div>
+  );
+}
+
+export default App;
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>안녕</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.join(__dirname, '/docs'),
+    filename: 'bundle.js',
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /.js$/, // .js로 끝나는 모든 파일들
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
